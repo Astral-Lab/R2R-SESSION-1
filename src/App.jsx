3594c6a920068e7d86d6ad158c866ef3106442d4
astral labs <spacexplorer4@outlook.com>
2025-01-02 18:57:35 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -39,26 +39,31 @@ export default function App() {
             title={"Session 1"}
             desc={"Learn about the fundamentals of components, JSX, and state. You'll build your first React app by combining these core concepts and get an introduction to React fibre, the powerful rendering engine that enables rapid UI updates."}
             keywords={["Components", "JSX", "State", "React Fibre"]}
+            url={"https://github.com/Astral-Lab/R2R-SESSION-1"}
           />
           <CourseCard 
             title={"Session 2"}
             desc={"Learn how to render dyanmic lists, manage and understand keys, and use controlled forms in React. Build a flashcard app to put these concepts to practice and gain more experience on state management by creating interactive components."}
             keywords={["Lists", "Keys", "Handlers", "Controlled Forms"]}
+            url={"https://github.com/Astral-Lab/R2R-SESSION-2"}
           />
           <CourseCard 
             title={"Session 3"}
             desc={"Learn how to handle side effects and data fetching in React during this two-part session. In part 1, you will gain a clear understanding of side effects, the dependency array, and how it relates to data fetching. We begin building the Wordle clone to apply these concepts."}
             keywords={["Side effects", "Dependencies", "Data fetching"]}
+            url={"https://github.com/Astral-Lab/R2R-SESSION-3-4"}
           />
           <CourseCard 
             title={"Session 4"}
             desc={"Learn about conditional rendering, referencing DOM elements, and web hosting with Cloudflare. In part 2, you will continue building on these concepts, complete the Wordle clone, and deploy it to the web."}
             keywords={["Conditional rendering", "References", "Hosting"]}
+            url={"https://github.com/Astral-Lab/R2R-SESSION-3-4"}
           />
           <CourseCard 
             title={"Session 5"}
             desc={"In the final session you will learn about routing, global state management, and be given a brief introduction to advanced React concepts and libraries, such as custom hooks, memoisation, and Redux. You will also learn where to go next to continue advancing your React skills."}
             keywords={["Routing", "Global state", "Advanced React"]}
+            url={"https://github.com/Astral-Lab/R2R-SESSION-5"}
           />
         </div>
       </main>
